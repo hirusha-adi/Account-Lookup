491b18242f8dc24fc0e87921609dbf695d5bd81b
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-30 09:51:42 -0800

    make main window

---
+++ Account Lookup/MainWindow.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Account_Lookup
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            searchToolStripMenuItem = new ToolStripMenuItem();
+            clearToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            guideToolStripMenuItem = new ToolStripMenuItem();
+            howItWorksToolStripMenuItem = new ToolStripMenuItem();
+            licenseToolStripMenuItem = new ToolStripMenuItem();
+            creditsToolStripMenuItem = new ToolStripMenuItem();
+            contributeToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, helpToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { searchToolStripMenuItem, clearToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // searchToolStripMenuItem
+            // 
+            searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            searchToolStripMenuItem.Size = new Size(180, 22);
+            searchToolStripMenuItem.Text = "Search";
+            searchToolStripMenuItem.Click += searchToolStripMenuItem_Click;
+            // 
+            // clearToolStripMenuItem
+            // 
+            clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            clearToolStripMenuItem.Size = new Size(180, 22);
+            clearToolStripMenuItem.Text = "Clear";
+            clearToolStripMenuItem.Click += clearToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(180, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { guideToolStripMenuItem, howItWorksToolStripMenuItem, licenseToolStripMenuItem, creditsToolStripMenuItem, contributeToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // guideToolStripMenuItem
+            // 
+            guideToolStripMenuItem.Name = "guideToolStripMenuItem";
+            guideToolStripMenuItem.Size = new Size(180, 22);
+            guideToolStripMenuItem.Text = "Guide";
+            guideToolStripMenuItem.Click += guideToolStripMenuItem_Click;
+            // 
+            // howItWorksToolStripMenuItem
+            // 
+            howItWorksToolStripMenuItem.Name = "howItWorksToolStripMenuItem";
+            howItWorksToolStripMenuItem.Size = new Size(180, 22);
+            howItWorksToolStripMenuItem.Text = "How it works?";
+            howItWorksToolStripMenuItem.Click += howItWorksToolStripMenuItem_Click;
+            // 
+            // licenseToolStripMenuItem
+            // 
+            licenseToolStripMenuItem.Name = "licenseToolStripMenuItem";
+            licenseToolStripMenuItem.Size = new Size(180, 22);
+            licenseToolStripMenuItem.Text = "License";
+            licenseToolStripMenuItem.Click += licenseToolStripMenuItem_Click;
+            // 
+            // creditsToolStripMenuItem
+            // 
+            creditsToolStripMenuItem.Name = "creditsToolStripMenuItem";
+            creditsToolStripMenuItem.Size = new Size(180, 22);
+            creditsToolStripMenuItem.Text = "Credits";
+            creditsToolStripMenuItem.Click += creditsToolStripMenuItem_Click;
+            // 
+            // contributeToolStripMenuItem
+            // 
+            contributeToolStripMenuItem.Name = "contributeToolStripMenuItem";
+            contributeToolStripMenuItem.Size = new Size(180, 22);
+            contributeToolStripMenuItem.Text = "Contribute";
+            contributeToolStripMenuItem.Click += contributeToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(52, 20);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "MainWindow";
+            Text = "Account Lookup";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem searchToolStripMenuItem;
+        private ToolStripMenuItem clearToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem guideToolStripMenuItem;
+        private ToolStripMenuItem howItWorksToolStripMenuItem;
+        private ToolStripMenuItem licenseToolStripMenuItem;
+        private ToolStripMenuItem creditsToolStripMenuItem;
+        private ToolStripMenuItem contributeToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
---
+++ Account Lookup/MainWindow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Account_Lookup
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void searchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void howItWorksToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void licenseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void creditsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contributeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
