4a8398d862b9f6c07595da1d8de921052185e207
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2023-12-30 12:16:48 -0800

    make it ordered

--- MainWindow.py
+++ MainWindow.py
@@ -10,9 +10,13 @@ import sys
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+        
+        _translate = QtCore.QCoreApplication.translate
+        
         # MainWindow setup
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(381, 491)
+        MainWindow.setWindowTitle(_translate("MainWindow", "Account Lookup"))
 
         # Central Widget setup
         self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
@@ -25,16 +29,19 @@ class Ui_MainWindow(object):
         font.setPointSize(20)
         self.label_title.setFont(font)
         self.label_title.setObjectName("label_title")
+        self.label_title.setText(_translate("MainWindow", "Account Lookup"))
 
         # Search Group setup
         self.group_search = QtWidgets.QGroupBox(parent=self.centralwidget)
         self.group_search.setGeometry(QtCore.QRect(10, 60, 361, 71))
         self.group_search.setObjectName("group_search")
+        self.group_search.setTitle(_translate("MainWindow", "Search"))
 
         # Username Label setup
         self.label_username = QtWidgets.QLabel(parent=self.group_search)
         self.label_username.setGeometry(QtCore.QRect(20, 30, 61, 31))
         self.label_username.setObjectName("label_username")
+        self.label_username.setText(_translate("MainWindow", "Username:"))
 
         # Username TextEdit setup
         self.txt_username = QtWidgets.QTextEdit(parent=self.group_search)
@@ -45,11 +52,13 @@ class Ui_MainWindow(object):
         self.btn_search = QtWidgets.QPushButton(parent=self.group_search)
         self.btn_search.setGeometry(QtCore.QRect(220, 30, 75, 31))
         self.btn_search.setObjectName("btn_search")
+        self.btn_search.setText(_translate("MainWindow", "Search"))
 
         # Clear Button setup
         self.btn_clear = QtWidgets.QPushButton(parent=self.group_search)
         self.btn_clear.setGeometry(QtCore.QRect(300, 30, 31, 31))
         self.btn_clear.setObjectName("btn_clear")
+        self.btn_clear.setText(_translate("MainWindow", "X"))
 
         # Table Widget setup
         self.table_widget = QtWidgets.QTableWidget(parent=self.centralwidget)
@@ -57,6 +66,7 @@ class Ui_MainWindow(object):
         self.table_widget.setObjectName("table_widget")
         self.table_widget.setColumnCount(3)
         self.table_widget.setRowCount(0)
+        item = self.table_widget.horizontalHeaderItem(0)
 
         # Table Headers setup
         item = QtWidgets.QTableWidgetItem()
@@ -65,15 +75,23 @@ class Ui_MainWindow(object):
         self.table_widget.setHorizontalHeaderItem(1, item)
         item = QtWidgets.QTableWidgetItem()
         self.table_widget.setHorizontalHeaderItem(2, item)
+        
+        item.setText(_translate("MainWindow", "ID"))
+        item = self.table_widget.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Platform Name"))
+        item = self.table_widget.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Status"))
 
-        # Labels setup
+        # Labels setup - Bottom
         self.label_3 = QtWidgets.QLabel(parent=self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(10, 430, 71, 16))
         self.label_3.setObjectName("label_3")
+        self.label_3.setText(_translate("MainWindow", "Exit"))
 
         self.label_4 = QtWidgets.QLabel(parent=self.centralwidget)
         self.label_4.setGeometry(QtCore.QRect(260, 430, 111, 20))
         self.label_4.setObjectName("label_4")
+        self.label_4.setText(_translate("MainWindow", "Made by @hirushaadi"))
 
         # Set Central Widget
         MainWindow.setCentralWidget(self.centralwidget)
@@ -86,10 +104,15 @@ class Ui_MainWindow(object):
         # Menu setup
         self.menuFile = QtWidgets.QMenu(parent=self.menubar)
         self.menuFile.setObjectName("menuFile")
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        
         self.menuGuide = QtWidgets.QMenu(parent=self.menubar)
         self.menuGuide.setObjectName("menuGuide")
+        self.menuGuide.setTitle(_translate("MainWindow", "Help"))
+        
         self.menuAbout = QtWidgets.QMenu(parent=self.menubar)
         self.menuAbout.setObjectName("menuAbout")
+        self.menuAbout.setTitle(_translate("MainWindow", "About"))
 
         # Set Menu Bar
         MainWindow.setMenuBar(self.menubar)
@@ -102,28 +125,36 @@ class Ui_MainWindow(object):
         # Action setup
         self.actionSearch = QtGui.QAction(parent=MainWindow)
         self.actionSearch.setObjectName("actionSearch")
+        self.actionSearch.setText(_translate("MainWindow", "Search"))
 
         self.actionClear = QtGui.QAction(parent=MainWindow)
         self.actionClear.setObjectName("actionClear")
+        self.actionClear.setText(_translate("MainWindow", "Clear"))
 
         self.actionExit = QtGui.QAction(parent=MainWindow)
         self.actionExit.setObjectName("actionExit")
         self.actionExit.triggered.connect(sys.exit)  # Connect Exit action to sys.exit
+        self.actionExit.setText(_translate("MainWindow", "Exit"))
 
         self.actionGuide = QtGui.QAction(parent=MainWindow)
         self.actionGuide.setObjectName("actionGuide")
+        self.actionGuide.setText(_translate("MainWindow", "Guide"))
 
         self.actionHow_it_works = QtGui.QAction(parent=MainWindow)
         self.actionHow_it_works.setObjectName("actionHow_it_works")
+        self.actionHow_it_works.setText(_translate("MainWindow", "How it works?"))
 
         self.actionLicense = QtGui.QAction(parent=MainWindow)
         self.actionLicense.setObjectName("actionLicense")
+        self.actionLicense.setText(_translate("MainWindow", "License"))
 
         self.actionCredits = QtGui.QAction(parent=MainWindow)
         self.actionCredits.setObjectName("actionCredits")
+        self.actionCredits.setText(_translate("MainWindow", "Credits"))
 
         self.actionContribute = QtGui.QAction(parent=MainWindow)
         self.actionContribute.setObjectName("actionContribute")
+        self.actionContribute.setText(_translate("MainWindow", "Contribute"))
 
         # Add actions to menus
         self.menuFile.addAction(self.actionSearch)
@@ -142,35 +173,5 @@ class Ui_MainWindow(object):
         self.menubar.addAction(self.menuAbout.menuAction())
 
         # Translate UI elements
-        self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-
-        # Set titles and labels
-        MainWindow.setWindowTitle(_translate("MainWindow", "Account Lookup"))
-        self.label_title.setText(_translate("MainWindow", "Account Lookup"))
-        self.group_search.setTitle(_translate("MainWindow", "Search"))
-        self.label_username.setText(_translate("MainWindow", "Username:"))
-        self.btn_search.setText(_translate("MainWindow", "Search"))
-        self.btn_clear.setText(_translate("MainWindow", "X"))
-        item = self.table_widget.horizontalHeaderItem(0)
-        item.setText(_translate("MainWindow", "ID"))
-        item = self.table_widget.horizontalHeaderItem(1)
-        item.setText(_translate("MainWindow", "Platform Name"))
-        item = self.table_widget.horizontalHeaderItem(2)
-        item.setText(_translate("MainWindow", "Status"))
-        self.label_3.setText(_translate("MainWindow", "Exit"))
-        self.label_4.setText(_translate("MainWindow", "Made by @hirushaadi"))
-        self.menuFile.setTitle(_translate("MainWindow", "File"))
-        self.menuGuide.setTitle(_translate("MainWindow", "Help"))
-        self.menuAbout.setTitle(_translate("MainWindow", "About"))
-        self.actionSearch.setText(_translate("MainWindow", "Search"))
-        self.actionClear.setText(_translate("MainWindow", "Clear"))
-        self.actionExit.setText(_translate("MainWindow", "Exit"))
-        self.actionGuide.setText(_translate("MainWindow", "Guide"))
-        self.actionHow_it_works.setText(_translate("MainWindow", "How it works?"))
-        self.actionLicense.setText(_translate("MainWindow", "License"))
-        self.actionCredits.setText(_translate("MainWindow", "Credits"))
-        self.actionContribute.setText(_translate("MainWindow", "Contribute"))
